058b50f78d7ae128e1554ef3dac5ab269e815bc1
aml esmail <aml.abdelbaki91@gmail.com>
2026-04-07 13:37:43 +0200

    format task board

--- src/app/features/dashboard/task-board/task-board.component.ts
+++ src/app/features/dashboard/task-board/task-board.component.ts
@@ -6,78 +6,50 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-task-board',
   standalone: true,
-  imports: [
-    TaskColumnComponent,
-    CommonModule,
-    DragDropModule
-  ],
+  imports: [TaskColumnComponent, CommonModule, DragDropModule],
   templateUrl: './task-board.component.html',
   styleUrl: './task-board.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TaskBoardComponent {
   @Input() tasks: Task[] = [];
-  @Output() statusUpdated = new EventEmitter<{ task: Task, status: TaskStatus }>();
+  @Output() statusUpdated = new EventEmitter<{
+    task: Task;
+    status: TaskStatus;
+  }>();
 
-  columns: { key: TaskStatus, title: string }[] = [
+  columns: { key: TaskStatus; title: string }[] = [
     { key: 'todo', title: 'To Do' },
     { key: 'in_progress', title: 'In Progress' },
-    { key: 'done', title: 'Done' }
+    { key: 'done', title: 'Done' },
   ];
 
   getTasksByStatus(status: string) {
-    return this.tasks.filter(t => t.status === status);
+    return this.tasks.filter((t) => t.status === status);
   }
 
-drop(data: { event: CdkDragDrop<Task[]>, status: TaskStatus }) {
-
-
-   const event = data.event;
+  drop(data: { event: CdkDragDrop<Task[]>; status: TaskStatus }) {
+    const event = data.event;
     if (event.previousContainer !== event.container) {
-          transferArrayItem(
-            event.previousContainer.data,
-            event.container.data,
-            event.previousIndex,
-            event.currentIndex
-           );
-          const task = data.event.item.data;
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      const task = data.event.item.data;
 
-          this.statusUpdated.emit({
-            task,
-            status: data.status
-          });
+      this.statusUpdated.emit({
+        task,
+        status: data.status,
+      });
     }
+  }
 
 
-}
-
-
-// drop(data: { event: CdkDragDrop<Task[]>, status: TaskStatus }) {
-//   const event = data.event;
-
-//   if (event.previousContainer !== event.container) {
-
-//     transferArrayItem(
-//       event.previousContainer.data,
-//       event.container.data,
-//       event.previousIndex,
-//       event.currentIndex
-//     );
-
-//     // 💥 حدّثي status
-//     const task = event.container.data[event.currentIndex];
-//     task.status = data.status;
-
-//     // لو signals
-//     this.tasks.update(tasks => [...tasks]);
-//   }
-// }
-
-
-
-getConnectedList(){
-  return this.columns.map(c => c.key);
-}
+  getConnectedList() {
+    return this.columns.map((c) => c.key);
+  }
 }
 
 
