c844c15034706c957c8419368a9181f2ec0efb12
aml esmail <aml.abdelbaki91@gmail.com>
2026-04-07 13:34:59 +0200

    fetching dashboard data

--- src/app/features/dashboard/dashboard-page.component.ts
+++ src/app/features/dashboard/dashboard-page.component.ts
@@ -4,11 +4,15 @@ import { Statistic } from './models/statistic.interface';
 import { DashboardToolbarComponent } from './dashboard-toolbar/dashboard-toolbar.component';
 import { TaskBoardComponent } from './task-board/task-board.component';
 import { Task, TaskStatus } from './models/task.interface';
+import { ManageTaskService } from '../../shared/services/manage-task.service';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [TaskCounterCardComponent, DashboardToolbarComponent,TaskBoardComponent],
+  imports: [
+    TaskCounterCardComponent,
+     DashboardToolbarComponent,
+     TaskBoardComponent],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.scss',
 })
@@ -32,47 +36,10 @@ export class DashboardPageComponent {
       isOverdue: false,
       completedAt: ''
     }]);
-  readonly stats: Statistic[] = [
-    {
-      id: '1',
-      title: 'Total Tasks',
-      value: 24,
-      icon: 'assignment',
-      changeLabel: '+12 this week',
-      changeType: 'positive',
-      color: 'blue',
-    },
-    {
-      id: '2',
-      title: 'Completed',
-      value: 18,
-      icon: 'check_circle',
-      changeLabel: '+8 today',
-      changeType: 'positive',
-      color: 'green',
-    },
-    {
-      id: '3',
-      title: 'In Progress',
-      value: 4,
-      icon: 'hourglass_empty',
-      changeLabel: 'Same as yesterday',
-      changeType: 'neutral',
-      color: 'grey',
-    },
-    {
-      id: '4',
-      title: 'Overdue',
-      value: 2,
-      icon: 'warning',
-      changeLabel: '+3 today',
-      changeType: 'negative',
-      color: 'red',
-    },
-  ];
+   stats: Statistic[] = [];
 
 filterStatus = signal('all');
-filterPriority = signal('');
+filterPriority = signal('all');
 
 onStatusChange(status: string) {
   this.filterStatus.set(status);
@@ -95,9 +62,22 @@ filteredTasks = computed(() => {
   });
 });
 
+constructor(private manageTaskService: ManageTaskService) {
+}
+
 ngOnInit() {
+  this.getDashboardData();
 
 
+}
+
+getDashboardData() {
+  this.manageTaskService.getDashboardData().subscribe((data: any) => {
+    console.log(data,'Dashboard Data');
+    this.tasks.set(data.tasks);
+    this.stats = data.statistics;
+
+  });
 }
 //  addDays(date, days) {
 //   const result = new Date(date);
